beb10b455577c0913cc78b60b1a84f6ec75c471b
Helio Junior <93434324+HelioDevv@users.noreply.github.com>
2024-02-07 20:46:52 -0300

    part 1 - Domain model

---
+++ src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato: ");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Data (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Valor do contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, date, totalValue);
+		
+		System.out.print("Entre com o número de parcelas: ");
+		int n = sc.nextInt();
+		
+		System.out.println("Parcelas: ");
+		for(int i = 0; i < n; i++) {
+			
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/model/services/PaypalService.java
@@ -0,0 +1,11 @@
+package model.services;
+
+public class PaypalService implements OnlinePaymentService {
+
+	public double paymentFee(double amount) {
+		return 0;
+	}
+	public double interest(double amount, int months) {
+		return 0;
+	}
+}
